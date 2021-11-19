eaba525d31ce2008f2995f8ac754b130fd5c8748
Nelson Junior <dudu.da.mata@hotmail.com>
2021-11-18 23:26:23 -0300

    iniciando a criacao de rotas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedex/common/repositories/pokemon_repository.dart';
-import 'features/home/container/home_container.dart';
+import 'package:pokedex/features/pokedex/route.dart';
+//import 'features/home/container/home_container.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,11 +17,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: HomeContainer(
+      home: PokedexRoute(
         repository: PokemonRepository(
           dio: Dio(),
         ),
       ),
+
+
+      /*home: HomeContainer(
+        repository: PokemonRepository(
+          dio: Dio(),
+        ),*/
+      
     );
   }
 }
