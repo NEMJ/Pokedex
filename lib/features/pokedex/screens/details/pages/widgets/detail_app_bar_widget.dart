f03a6dc276900ee67aac42a1f4f70e1831928d9c
Nelson Junior <dudu.da.mata@hotmail.com>
2021-12-13 12:33:02 -0300

    animations on the detail page

--- lib/features/pokedex/screens/details/pages/widgets/detail_app_bar_widget.dart
+++ lib/features/pokedex/screens/details/pages/widgets/detail_app_bar_widget.dart
@@ -6,19 +6,35 @@ class DetailAppBarWidget extends StatelessWidget {
     Key? key,
     required this.pokemon,
     required this.onBack,
+    required this.isOnTop,
   }) : super(key: key);
 
   final Pokemon pokemon;
   final VoidCallback onBack;
+  final bool isOnTop;
 
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(    
+    return SliverAppBar(
+      pinned: true,
       elevation: 0,
       backgroundColor: pokemon.baseColor,
       leading: IconButton(
         onPressed: onBack,
         icon: Icon(Icons.chevron_left),
+      ),
+      centerTitle: false,
+      title: AnimatedOpacity(
+        duration: Duration(milliseconds: 300),
+        opacity: isOnTop ? 0 : 1,
+        child: Text(
+          pokemon.name,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
